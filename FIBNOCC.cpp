c762404682c4d01b811ec57bdbffa642cb607906
ch lakshmi narasimha <cv8516499@gmail.com>
2025-11-26 13:43:44 +0530

    Add files via upload

---
+++ FIBNOCC.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int fibonacci(int);
+int main() 
+{ 	int n,i;
+ 	printf("Enter the number of terms in the Fibonacci sequence: ");
+ 	scanf("%d", &n);
+ 	printf("Fibonacci Sequence:\n");
+ 	for (i = 0; i < n; i++)
+	  {
+ 		printf("%d ", fibonacci(i));
+ 	}
+ return 0;
+}
+int fibonacci(int n) 
+{	if (n <= 1) 
+	{ 		return n; 	}
+	else 
+	 {
+ 		return fibonacci(n - 1) + fibonacci(n - 2);
+ 	}
+}
